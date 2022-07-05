b179fbb07a174971c46540068fbafd5011015456
ductam2411 <ductam2943@gmail.com>
2022-07-05 11:31:15 +0700

    add api

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,7 +10,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
     method: "GET",
     headers: {
-        "X-RapidAPI-Key": "50875597afmsh9372f64489a60aap171b3ejsn2b4ea10b1163",
+        "X-RapidAPI-Key": process.env.REACT_API_YOUTUBE,
         "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
     },
 };
