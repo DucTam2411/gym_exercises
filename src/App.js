65f54a5796156c0611b8c5f79f2cda381925aac7
ductam2411 <ductam2943@gmail.com>
2022-07-05 12:14:39 +0700

    a

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/exercise/:id" element={<ExerciseDetail />} />
             </Routes>
-            <Footer />
         </Box>
     );
 };
